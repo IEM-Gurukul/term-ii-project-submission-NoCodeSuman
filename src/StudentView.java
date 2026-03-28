1b73bc611e8e5bce73ee1e2c4c0c52f70364d1c2
NoCodeSuman <kundusuman824@gmail.com>
2026-03-28 09:29:48 +0530

    StudentView added

--- src/StudentView.java
+++ src/StudentView.java
@@ -3,4 +3,13 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
+public class StudentView {
+    private JFrame frame;
+    private JTextField nameField;
+    private JComboBox<String> courseBox;
+    private JTable table;
+    private DefaultTableModel tableModel;
 
+    private JButton saveButton;
+    private JButton clearButton;
+    private JButton searchButton;
