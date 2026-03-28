77fd1ca06164f95270a5a6660039989ccda1761a
NoCodeSuman <kundusuman824@gmail.com>
2026-03-28 09:26:08 +0530

    StudentRepository added

--- src/StudentRepository.java
+++ src/StudentRepository.java
@@ -16,4 +16,27 @@ public class StudentRepository {
         }
     }
 
+    public List<Student> load() {
+        List<Student> students = new ArrayList<>();
+        File file = new File(fileName);
+        if (!file.exists()) {
+            return students;
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data.length == 2) {
+                    students.add(new Student(data[0], data[1]));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
+}
+
+
     
