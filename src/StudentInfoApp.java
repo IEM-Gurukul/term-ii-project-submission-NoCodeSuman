fd09e76f0c31e7ad5b45cf5039d5bb992a97228a
NoCodeSuman <kundusuman824@gmail.com>
2026-03-28 09:43:21 +0530

    StudentInfoApp added

--- src/StudentInfoApp.java
+++ src/StudentInfoApp.java
@@ -1,16 +1,14 @@
-// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+// StudentInfoApp.java
 import javax.swing.SwingUtilities;
 
 public class StudentInfoApp {
-   public StudentInfoApp() {
-   }
-
-   public static void main(String[] var0) {
-      SwingUtilities.invokeLater(() -> {
-         StudentView var0 = new StudentView();
-         StudentRepository var1 = new StudentRepository();
-         new StudentController(var0, var1);
-         var0.setVisible(true);
-      });
-   }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            StudentView view = new StudentView();
+            StudentRepository repository = new StudentRepository();
+            new StudentController(view, repository);
+            view.setVisible(true);
+        });
+    }
 }
+
